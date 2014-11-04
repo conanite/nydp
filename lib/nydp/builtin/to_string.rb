4f58231452fc3687534eecfc7d54c26f9de2e34e
conanite <conan@conandalton.net>
2014-11-04 14:51:29 +0100

    strings: #to-string always returns StringAtom

--- lib/nydp/builtin/to_string.rb
+++ lib/nydp/builtin/to_string.rb
@@ -2,10 +2,10 @@ class Nydp::Builtin::ToString
   def invoke vm, args
     arg = args.car
     val = case arg.class
-          when String, Nydp::StringAtom
+          when Nydp::StringAtom
             arg
           else
-            arg.to_s
+            Nydp::StringAtom.new arg.to_s
           end
     vm.push_arg val
   end
