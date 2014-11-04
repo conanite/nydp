de1fc1bab6e9ed94a3a06f630608eb9b7f4e0ef5
conanite <conan@conandalton.net>
2014-11-04 14:53:05 +0100

    strings: override #+ for StringAtom, allow nil token in constructor, for instances that are built not by the parser

--- lib/nydp/string_atom.rb
+++ lib/nydp/string_atom.rb
@@ -1,7 +1,7 @@
 module Nydp
   class StringAtom
     attr_accessor :string, :token
-    def initialize string, token
+    def initialize string, token=nil
       @string, @token = string, token
     end
 
@@ -10,11 +10,15 @@ module Nydp
     end
 
     def inspect
-      token.rep
+      token ? token.rep : string.inspect
     end
 
     def == other
       other.to_s == self.to_s
+    end
+
+    def + other
+      StringAtom.new "#{@string}#{other}"
     end
   end
 end
