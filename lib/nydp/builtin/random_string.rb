7312631fa80fbc60dfda8d0760b6bffadfcaaa9b
conanite <conan@conandalton.net>
2014-11-29 16:07:31 +0100

    random-string: oops

--- lib/nydp/builtin/random_string.rb
+++ lib/nydp/builtin/random_string.rb
@@ -1,4 +1,4 @@
-class RandomString
+class Nydp::Builtin::RandomString
   @@random_chars = ["A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z","2","3","4","5","6","7","8","9"]
 
   def invoke vm, args
@@ -8,3 +8,4 @@ class RandomString
     }
     Nydp::StringAtom.new s
   end
+end
