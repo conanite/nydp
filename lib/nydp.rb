1d1f09368c27c2083a25c0590b352dc778c5bab2
conanite <conan@conandalton.net>
2015-02-08 15:54:40 +0100

    runner: extract launcher code into its own class

--- lib/nydp.rb
+++ lib/nydp.rb
@@ -6,93 +6,32 @@ module Nydp
   def self.setup ns;        PLUGINS.each { |plg| plg.setup ns } ; end
   def self.loadfiles;       PLUGINS.map(&:loadfiles).flatten    ; end
   def self.testfiles;       PLUGINS.map(&:testfiles).flatten    ; end
-
-  def self.compile_and_eval vm, expr
-    vm.thread Pair.new(Compiler.compile(expr, Nydp.NIL), Nydp.NIL)
-  end
-
-  class Runner
-    attr_accessor :vm, :ns
-
-    def initialize vm, ns
-      @vm = vm
-      @ns = ns
-      @precompile = Symbol.mk(:"pre-compile", ns)
-      @quote      = Symbol.mk(:quote, ns)
-    end
-
-    def quote expr
-      Pair.from_list [@quote, expr]
-    end
-
-    def precompile expr
-      Pair.from_list [@precompile, quote(expr)]
-    end
-
-    def pre_compile expr
-      Nydp.compile_and_eval(vm, precompile(expr))
-    end
-
-    def evaluate expr
-      Nydp.compile_and_eval(vm, pre_compile(expr))
-    end
-  end
-
-  class StreamRunner < Runner
-    attr_accessor :stream, :parser
-
-    def initialize vm, ns, stream
-      super vm, ns
-      @stream = stream
-      @parser = Nydp::Parser.new(ns)
-      @tokens = Nydp::Tokeniser.new stream
-    end
-
-    def prompt *_
-    end
-
-    def run
-      res = Nydp.NIL
-      prompt
-      while !@tokens.finished
-        expr = parser.expression(@tokens)
-        unless expr.nil?
-          res = evaluate expr
-          prompt res
-        end
-      end
-      res
-    end
-  end
-
-  class Repl < StreamRunner
-    def prompt val=nil
-      puts val if val
-      print "nydp > "
-    end
+  def self.loadall vm, ns, files
+    files.each { |f| StreamRunner.new(vm, ns, File.new(f)).run }
   end
 
   def self.repl
     puts "welcome to nydp"
-    root_ns = { }
-    setup(root_ns)
+    ns = { }
+    setup(ns)
     vm = VM.new
-    self.loadfiles.each { |f| StreamRunner.new(vm, root_ns, File.new(f)).run }
-    Repl.new(vm, root_ns, $stdin).run
+    loadall vm, ns, loadfiles
+    Repl.new(vm, ns, $stdin).run
   end
 
   def self.tests
     puts "welcome to nydp : running tests"
-    root_ns = { }
-    setup(root_ns)
+    ns = { }
+    setup(ns)
     vm = VM.new
-    self.loadfiles.each { |f| StreamRunner.new(vm, root_ns, File.new(f)).run }
-    self.testfiles.each { |f| StreamRunner.new(vm, root_ns, File.new(f)).run }
-    StreamRunner.new(vm, root_ns, "(run-all-tests)").run
+    loadall vm, ns, loadfiles
+    loadall vm, ns, testfiles
+    StreamRunner.new(vm, ns, "(run-all-tests)").run
   end
 end
 
 require "nydp/core"
+require "nydp/runner"
 require "nydp/error"
 require "nydp/truth"
 require "nydp/version"
---
+++ lib/nydp/runner.rb
@@ -0,0 +1,66 @@
+module Nydp
+  class Runner
+    attr_accessor :vm, :ns
+
+    def initialize vm, ns
+      @vm = vm
+      @ns = ns
+      @precompile = Symbol.mk(:"pre-compile", ns)
+      @quote      = Symbol.mk(:quote, ns)
+    end
+
+    def compile_and_eval expr
+      vm.thread Pair.new(Compiler.compile(expr, Nydp.NIL), Nydp.NIL)
+    end
+
+    def quote expr
+      Pair.from_list [@quote, expr]
+    end
+
+    def precompile expr
+      Pair.from_list [@precompile, quote(expr)]
+    end
+
+    def pre_compile expr
+      compile_and_eval(precompile(expr))
+    end
+
+    def evaluate expr
+      compile_and_eval(pre_compile(expr))
+    end
+  end
+
+  class StreamRunner < Runner
+    attr_accessor :stream, :parser
+
+    def initialize vm, ns, stream
+      super vm, ns
+      @stream = stream
+      @parser = Nydp::Parser.new(ns)
+      @tokens = Nydp::Tokeniser.new stream
+    end
+
+    def prompt *_
+    end
+
+    def run
+      res = Nydp.NIL
+      prompt
+      while !@tokens.finished
+        expr = parser.expression(@tokens)
+        unless expr.nil?
+          res = evaluate expr
+          prompt res
+        end
+      end
+      res
+    end
+  end
+
+  class Repl < StreamRunner
+    def prompt val=nil
+      puts val if val
+      print "nydp > "
+    end
+  end
+end
