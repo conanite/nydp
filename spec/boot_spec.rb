4f0dd702407be9d82ed267c2b982e4316be7dbb3
conanite <conan@conandalton.net>
2015-05-15 11:48:02 +0200

    testing: move some more examples from rspec to lisp

--- spec/boot_spec.rb
+++ spec/boot_spec.rb
@@ -27,31 +27,11 @@ describe Nydp do
     Nydp::Runner.new(vm, ns, Nydp::StringReader.new(txt)).run
   end
 
-  it "should map a function over a list of numbers" do
-    expect(run "(map (fn (x) (* x x)) '(1 2 3))").to eq Nydp::Pair.from_list [1, 4, 9]
-  end
-
-  describe "pairs" do
-    it "should break a list into pairs" do
-      result   = run "(pairs '(1 a 2 b 3 c))"
-      expected = run "'((1 a) (2 b) (3 c))"
-      expect(result).to eq expected
-    end
-  end
-
   describe :let do
     it "should create an inner scope with a single variable" do
       lisp = "(def x+3*z (x) (let y 3 (fn (z) (* (+ x y) z)))) ((x+3*z 2) 5)"
       result = run lisp
       expect(result).to eq 25
-    end
-  end
-
-  describe :flatten do
-    it "should return a flat list of things" do
-      lisp = "(flatten '((poo (x) (* x x)) (1 2 3)))"
-      result = run lisp
-      expect(result).to eq list :poo, :x, :"*", :x, :x, 1, 2, 3
     end
   end
 
