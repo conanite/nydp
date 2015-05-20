222382d2eabb9b3abf4a68fa1788e9b3238e9fa3
conanite <conan@conandalton.net>
2015-05-20 12:08:54 +0200

    errors: move #indent_text to error.rb for general use

--- lib/nydp/builtin.rb
+++ lib/nydp/builtin.rb
@@ -1,15 +1,12 @@
 require 'nydp'
+require 'nydp/error'
 
 module Nydp::Builtin
   module Base
-    def indent_text txt
-      txt.split(/\n/).map { |line| "  #{line}"}.join("\n")
-    end
-
     def invoke vm, args
       builtin_invoke vm, args
     rescue Exception => e
-      new_msg = "Invoking #{self.class.name}\nwith args #{args}\nraised\n#{indent_text e.message}"
+      new_msg = "Invoking #{self.class.name}\nwith args #{args}\nraised\n#{Nydp.indent_text e.message}"
       raise $!, new_msg, $!.backtrace
     end
   end
--- lib/nydp/error.rb
+++ lib/nydp/error.rb
@@ -1,4 +1,8 @@
 module Nydp
+  def self.indent_text txt
+    txt.split(/\n/).map { |line| "  #{line}"}.join("\n")
+  end
+
   class Error < StandardError
   end
 end
