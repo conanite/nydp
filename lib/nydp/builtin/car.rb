78b7796687eac3cbf145af6974b01cc8f30e828f
conanite <conan@conandalton.net>
2015-07-22 10:21:54 -0400

    repl: improve debug names for builtins

--- lib/nydp/builtin/car.rb
+++ lib/nydp/builtin/car.rb
@@ -2,4 +2,7 @@ class Nydp::Builtin::Car
   def invoke vm, args
     vm.push_arg args.car.car
   end
+
+  def to_s; "car"; end
+  def inspect; "builtin:car"; end
 end
--- lib/nydp/builtin/cdr.rb
+++ lib/nydp/builtin/cdr.rb
@@ -2,4 +2,7 @@ class Nydp::Builtin::Cdr
   def invoke vm, args
     vm.push_arg args.car.cdr
   end
+
+  def to_s; "cdr"; end
+  def inspect; "builtin:cdr"; end
 end
