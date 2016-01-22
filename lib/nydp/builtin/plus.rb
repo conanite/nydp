75d21500bb487029b4e14c63b98c46356c5f2b6c
conanite <conan@conandalton.net>
2016-01-22 12:39:12 +0100

    performance: faster 'plus

--- lib/nydp/builtin/plus.rb
+++ lib/nydp/builtin/plus.rb
@@ -1,6 +1,11 @@
 class Nydp::Builtin::Plus
   include Nydp::Builtin::Base
 
+  def builtin_invoke_1 vm             ; vm.push_arg 0             ; end
+  def builtin_invoke_2 vm, a          ; vm.push_arg a             ; end
+  def builtin_invoke_3 vm, a0, a1     ; vm.push_arg(a0 + a1)      ; end
+  def builtin_invoke_4 vm, a0, a1, a2 ; vm.push_arg(a0 + a1 + a2) ; end
+
   def builtin_invoke vm, args
     vm.push_arg case args.car
                 when Nydp::Pair
