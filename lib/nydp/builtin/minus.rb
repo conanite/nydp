5449471a0b935ad178c65f5a198254e3a3feec27
conanite <conan@conandalton.net>
2016-01-22 13:25:21 +0100

    performance: optimise 'minus

--- lib/nydp/builtin/minus.rb
+++ lib/nydp/builtin/minus.rb
@@ -1,6 +1,11 @@
 class Nydp::Builtin::Minus
   include Nydp::Builtin::Base
 
+  def invoke_1 vm             ; vm.push_arg 0             ; end
+  def invoke_2 vm, a          ; vm.push_arg -a            ; end
+  def invoke_3 vm, a0, a1     ; vm.push_arg(a0 - a1)      ; end
+  def invoke_4 vm, a0, a1, a2 ; vm.push_arg(a0 - a1 - a2) ; end
+
   def builtin_invoke vm, args
     vm.push_arg diff(args.cdr, args.car)
   end
