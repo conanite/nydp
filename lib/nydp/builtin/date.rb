8eaedf3045b1c3f9209eadf07db05ecf72ea97cc
conanite <conan@conandalton.net>
2016-05-26 13:53:24 +0200

    dates: more generally useful 'date builtin ; remove 'today builtin, replaced by zero-arg 'date

---
+++ lib/nydp/builtin/date.rb
@@ -0,0 +1,20 @@
+class Nydp::Builtin::Date
+  include Nydp::Helper, Nydp::Builtin::Base
+
+  def builtin_invoke_1 vm
+    vm.push_arg(Nydp::Date.new Date.today)
+  end
+
+  # it's a Time object (or any object that responds to #to_date)
+  def builtin_invoke_2 vm, arg
+    vm.push_arg(Nydp::Date.new args.car.to_date)
+  end
+
+  def builtin_invoke_3 vm, a0, a1
+    raise Nydp::Error.new "Date, got 2 args (#{a0} #{a1}), expected 0 or 1 or 3 args"
+  end
+
+  def builtin_invoke_4 vm, y, m, d
+    vm.push_arg(Nydp::Date.new Date.new(y,m,d))
+  end
+end
--- lib/nydp/core.rb
+++ lib/nydp/core.rb
@@ -58,7 +58,6 @@ module Nydp
       Symbol.mk("string-replace" , ns).assign(Nydp::Builtin::StringReplace.new)
       Symbol.mk("string-match"   , ns).assign(Nydp::Builtin::StringMatch.new(ns))
       Symbol.mk("string-split"   , ns).assign(Nydp::Builtin::StringSplit.new  )
-      Symbol.mk("today"          , ns).assign(Nydp::Builtin::Today.new)
       Symbol.mk("thread-locals"  , ns).assign(Nydp::Builtin::ThreadLocals.new)
       Symbol.mk("type-of",      ns).assign(Nydp::Builtin::TypeOf.new(ns))
       Symbol.mk(:"eq?",         ns).assign(Nydp::Builtin::IsEqual.new)
