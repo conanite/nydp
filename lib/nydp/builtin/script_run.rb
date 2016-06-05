4bebf18ff73b8015ad411c9b3f3951aa5d6916ff
conanite <conan@conandalton.net>
2016-06-05 15:03:46 +0200

    dox: replace builtin 'script-run with placeholder, create override 'script-run in nydp

--- lib/nydp/builtin/script_run.rb
+++ lib/nydp/builtin/script_run.rb
@@ -2,23 +2,6 @@ class Nydp::Builtin::ScriptRun
   include Nydp::Builtin::Base, Singleton
 
   def builtin_invoke vm, args
-    builtin_invoke_3 vm, args.car, args.cdr.car
-  end
-
-  def builtin_invoke_3 vm, event, name
-    puts "script-run: #{event.inspect} #{name.inspect}"
-    script_sym = Nydp::Symbol.mk :"script-name", vm.ns
-    plugin_sym = Nydp::Symbol.mk :"script-name", vm.ns
-    case event.to_sym
-    when :"script-start"
-      script_sym.assign(name)
-    when :"script-end"
-      script_sym.assign(Nydp::NIL)
-    when :"plugin-start"
-      plugin_sym.assign(name)
-    when :"plugin-end"
-      plugin_sym.assign(Nydp::NIL)
-    end
-    vm.push_arg name
+    vm.push_arg args
   end
 end
