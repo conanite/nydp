ff5d3cf561285c312c2723beeb886e34240b77f6
conanite <conan@conandalton.net>
2016-06-16 13:51:52 +0200

    performance: remove unused code from LexicalContext

--- lib/nydp/lexical_context.rb
+++ lib/nydp/lexical_context.rb
@@ -29,32 +29,6 @@ module Nydp
       values << value
     end
 
-    def set_args_1 names, arg
-      if pair? names
-        set names.car, arg
-      elsif Nydp::NIL.isnt? names
-        set names, cons(arg)
-      end
-    end
-
-    def set_args_2 names, arg_0, arg_1
-      if pair? names
-        set names.car, arg_0
-        set_args_1 names.cdr, arg_1
-      elsif Nydp::NIL.isnt? names
-        set names, cons(arg_0, cons(arg_1))
-      end
-    end
-
-    def set_args_3 names, arg_0, arg_1, arg_2
-      if pair? names
-        set names.car, arg_0
-        set_args_2 names.cdr, arg_1, arg_2
-      elsif Nydp::NIL.isnt? names
-        set names, cons(arg_0, cons(arg_1, cons(arg_2)))
-      end
-    end
-
     def set_index index, value
       values[index] = value
     end
