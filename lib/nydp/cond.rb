49305820ce45e53e40e4998583efe0002812c527
conanite <conan@conandalton.net>
2017-01-29 00:01:28 +0100

    performance: slight improvement for 'cond

--- lib/nydp/cond.rb
+++ lib/nydp/cond.rb
@@ -11,10 +11,10 @@ module Nydp
     end
 
     def inspect
-      "when_true:#{when_true.inspect}:when_false:#{when_false.inspect}"
+      "when_true:#{@when_true.inspect}:when_false:#{@when_false.inspect}"
     end
     def to_s
-      "#{when_true.car.to_s} #{when_false.car.to_s}"
+      "#{@when_true.to_s} #{@when_false.to_s}"
     end
   end
 
@@ -37,7 +37,7 @@ module Nydp
       "cond:#{condition.inspect}:#{conditional.inspect}"
     end
     def to_s
-      "(cond #{condition.car.to_s} #{conditional.to_s})"
+      "(cond #{condition.to_s} #{conditional.to_s})"
     end
 
     def self.build expressions, bindings
@@ -46,20 +46,12 @@ module Nydp
         when_true  = Compiler.compile expressions.cdr.car, bindings
         when_false = Compiler.compile expressions.cdr.cdr.car, bindings
         csig       = sig(cond)
-        tsig       = sig(when_true)
-        fsig       = sig(when_false)
-        cond_sig = "#{csig}_#{tsig}_#{fsig}"
         # puts cond_sig
+        # TODO : handle literal nil explicitly (if x y) -> #when_false is literal nil, we can hardcode that
+        # todo : handle "OR" explicitly -> (if x x y) -> when #cond equals #when_true, hardcode this case
         case csig
         when "LEX"
-          case cond_sig
-          when "LEX_LEX_LIT"
-            Cond_LEX_LEX_LIT.new(cond, when_true, when_false.expression)
-          when "LEX_CND_LIT"
-            Cond_LEX_CND_LIT.new(cond, when_true, when_false.expression)
-          else
-            Cond_LEX.new(cond, cons(when_true), cons(when_false))
-          end
+          Cond_LEX.build(cond, when_true, when_false)
         when "SYM"
           Cond_SYM.new(cond, cons(when_true), cons(when_false))
         else
@@ -89,7 +81,67 @@ module Nydp
       vm.instructions.push (truth ? @when_true : @when_false)
       vm.contexts.push vm.current_context
     end
+
+    def self.build cond, when_true, when_false
+      tsig       = sig(when_true)
+      fsig       = sig(when_false)
+      cond_sig = "#{tsig}_#{fsig}"
+
+      # if (cond == when_true)
+      #   OR_LEX.build cond, when_false
+
+      if (cond == when_true) && (fsig == "LIT")
+        OR_LEX_LIT.new cond, nil, when_false
+      elsif (cond == when_true) && (fsig == "LEX")
+        OR_LEX_LEX.new cond, nil, when_false
+      else
+        case cond_sig
+        when "LEX_LIT"
+          Nydp::Cond_LEX_LEX_LIT.new(cond, when_true, when_false.expression)
+        when "CND_LIT"
+          Nydp::Cond_LEX_CND_LIT.new(cond, when_true, when_false.expression)
+        else
+          Nydp::Cond_LEX.new(cond, cons(when_true), cons(when_false))
+        end
+      end
+    end
   end
+
+  class OR_LEX_LIT < CondBase
+    def execute vm
+      value = @condition.value vm.current_context
+      vm.push_arg(!Nydp::NIL.is?(value) ? value : @when_false)
+    end
+  end
+
+  class OR_LEX_LEX < CondBase
+    def execute vm
+      value = @condition.value vm.current_context
+      vm.push_arg(!Nydp::NIL.is?(value) ? value : (@when_false.value vm.current_context))
+    end
+  end
+
+  # class OR_LEX < CondBase
+  #   def execute vm
+  #     value = @condition.value vm.current_context
+  #     if !Nydp::NIL.is?(value)
+  #       vm.push_arg value
+  #     else
+  #       @when_false.execute vm
+  #     end
+  #   end
+
+  #   def self.build cond, when_false
+  #     case sig(when_false)
+  #     when "LIT"
+  #       OR_LEX_LIT.new(cond, nil, when_false)
+  #     when "LEX"
+  #       OR_LEX_LEX.new(cond, nil, when_false)
+  #     else
+  #       OR_LEX.new(cond, nil, when_false)
+  #     end
+  #   end
+  # end
 
   class Cond_LEX_LEX_LIT < CondBase
     def execute vm
