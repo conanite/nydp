bebc4497cf107335dbb52884b0c4b7b545d3a7f0
conanite <conan@conandalton.net>
2019-04-02 12:09:48 +0200

    strings: new pad-left and pad-right functions

---
+++ lib/nydp/builtin/string_pad_left.rb
@@ -0,0 +1,7 @@
+class Nydp::Builtin::StringPadLeft
+  include Nydp::Builtin::Base, Singleton
+
+  def builtin_invoke_4 vm, str, len, padding
+    vm.push_arg Nydp::StringAtom.new str.to_s.rjust len, padding.to_s
+  end
+end
---
+++ lib/nydp/builtin/string_pad_right.rb
@@ -0,0 +1,7 @@
+class Nydp::Builtin::StringPadRight
+  include Nydp::Builtin::Base, Singleton
+
+  def builtin_invoke_4 vm, str, len, padding
+    vm.push_arg Nydp::StringAtom.new str.to_s.ljust len, padding.to_s
+  end
+end
