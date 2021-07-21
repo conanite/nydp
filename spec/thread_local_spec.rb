1ef35ea53c8f147f74a53be8ddf605202bf6178c
conanite <conan@conandalton.net>
2021-07-21 06:37:01 +0200

    threading: VM is no longer relevant

--- spec/thread_local_spec.rb
+++ spec/thread_local_spec.rb
@@ -6,16 +6,32 @@ describe Nydp do
   end
 
   it "should isolate threadlocal values" do
+
+    # note : this test is mostly on principle, it can't guarantee
+    # correctness insofar as it doesn't guarantee that the threads
+    # run in a way such that they would interfere with each other
+    # if thread_locals were not used...
+
     Nydp.setup ns
 
-    vm0 = Nydp::VM.new ns
-    vm1 = Nydp::VM.new ns
+    vm = Nydp::VM.new ns
 
-    run vm0, "(hash-set (thread-locals) 'testing (+ 1 2 3))"
-    run vm1, "(hash-set (thread-locals) 'testing (+ 6 7 8))"
+    t0 = Thread.new {
+      run vm, "(hash-set (thread-locals) 'testing (+ 1 2 3))"
+      sleep 0.1
+      run vm, "(hash-get (thread-locals) 'testing)"
+    }
 
-    val0 = run vm0, "(hash-get (thread-locals) 'testing)"
-    val1 = run vm1, "(hash-get (thread-locals) 'testing)"
+    t1 = Thread.new {
+      run vm, "(hash-set (thread-locals) 'testing (+ 6 7 8))"
+      run vm, "(hash-get (thread-locals) 'testing)"
+    }
+
+    t0.join
+    t1.join
+
+    val0 = t0.value
+    val1 = t1.value
 
     expect(val0).to eq 6
     expect(val1).to eq 21
