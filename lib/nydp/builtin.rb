446388cc81e8fde402a3ee0f5d08bd4f898f840d
conanite <conan@conandalton.net>
2021-07-28 23:35:56 +0200

    compiler: remove #builtin_invoke methods and variations, using #call exclusively now

--- lib/nydp/builtin.rb
+++ lib/nydp/builtin.rb
@@ -2,43 +2,6 @@ module Nydp::Builtin
   module Base
     include Nydp::Helper
 
-    def builtin_invoke   vm, args       ; raise "#{self.class.name} : please implement #builtin_invoke" ; end
-    def builtin_invoke_1 vm             ; builtin_invoke vm, Nydp::NIL                    ; end
-    def builtin_invoke_2 vm, a          ; builtin_invoke vm, cons(a)                      ; end
-    def builtin_invoke_3 vm, a0, a1     ; builtin_invoke vm, cons(a0, cons(a1))           ; end
-    def builtin_invoke_4 vm, a0, a1, a2 ; builtin_invoke vm, cons(a0, cons(a1, cons(a2))) ; end
-
-    def invoke_1 vm
-      builtin_invoke_1 vm
-    rescue StandardError => e
-      handle_error e
-    end
-
-    def invoke_2 vm, arg
-      builtin_invoke_2 vm, arg
-    rescue StandardError => e
-      handle_error e, arg
-    end
-
-    def invoke_3 vm, arg_0, arg_1
-      builtin_invoke_3 vm, arg_0, arg_1
-    rescue StandardError => e
-      handle_error e, arg_0, arg_1
-    end
-
-    def invoke_4 vm, arg_0, arg_1, arg_2
-      builtin_invoke_4 vm, arg_0, arg_1, arg_2
-    rescue StandardError => e
-      handle_error e, arg_0, arg_1, arg_2
-    end
-
-    # called from 'apply (among others)
-    def invoke vm, args
-      builtin_invoke vm, args
-    rescue StandardError => e
-      handle_error e, *(args.to_a)
-    end
-
     def handle_error e, *args
       case e
       when Nydp::Error
